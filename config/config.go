ef8f8053d3f290f022c6c3d9f1c74a5fc0b7839f
bespontoff <bespontoff@gmail.com>
2022-06-09 01:00:15 +0300

    config interface

---
+++ config/config.go
@@ -0,0 +1,13 @@
+package config
+
+type Configuration interface {
+	GetString(name string) (value string, found bool)
+	GetInt(name string) (value int, found bool)
+	GetFloat(name string) (value float64, found bool)
+	GetBool(name string) (value bool, found bool)
+
+	GetStringDefault(name string, defaultValue string) (value string)
+	GetIntDefault(name string, defaultValue int) (value int)
+	GetFloatDefault(name string, defaultValue float64) (value float64)
+	GetBoolDefault(name string, defaultValue bool) (value bool)
+}
